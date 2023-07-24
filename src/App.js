5988f72499a98b0ff72e9c94fa3464f2c233f17a
Edinrovcanin <116107780+Edinrovcanin@users.noreply.github.com>
2023-07-24 22:34:37 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,8 +4,9 @@ import axios from 'axios';
 const App = () => {
   const [userInput, setUserInput] = useState('');
   const [selectedOption, setSelectedOption] = useState('Pravna pomoć 1');
+  const [selectedCountry, setSelectedCountry] = useState('Bosna i Hercegovina');
   const [chatGptResponse, setChatGptResponse] = useState('');
-  const apiKey = 'API MAKI OVDE PISIb';
+  const apiKey = 'UNESITE VAS API OVDE';
 
   const handleInputChange = (e) => {
     setUserInput(e.target.value);
@@ -15,13 +16,17 @@ const App = () => {
     setSelectedOption(e.target.value);
   };
 
+  const handleCountryChange = (e) => {
+    setSelectedCountry(e.target.value);
+  };
+
   const handleSendMessage = async () => {
     if (userInput.trim() !== '') {
       try {
         const response = await axios.post(
           'https://api.openai.com/v1/engines/text-davinci-003/completions',
           {
-            prompt: `Selected option: ${selectedOption}\nUser input: ${userInput}`,
+            prompt: `Selected option: ${selectedOption}\nSelected country: ${selectedCountry}\nUser input: ${userInput}`,
             max_tokens: 2000, // Povećan broj tokena kako bismo dobili duži odgovor max je 2048
           },
           {
@@ -56,6 +61,14 @@ const App = () => {
         {/* Dodati ostale pravne pomoći i opcije */}
       </select>
 
+      <h2 style={{ color: 'white', fontSize: '24px', marginBottom: '20px' }}>Odaberite državu</h2>
+      <select style={{ marginBottom: '20px', fontSize: '18px', height: '50px' }} value={selectedCountry} onChange={handleCountryChange}>
+        <option value="Bosna i Hercegovina">Bosna i Hercegovina</option>
+        <option value="Srbija">Srbija</option>
+        <option value="Hrvatska">Hrvatska</option>
+        <option value="Crna Gora">Crna Gora</option>
+      </select>
+
       <div style={{ display: 'flex', flexDirection: 'column', alignItems: 'center' }}>
         <input type="text" value={userInput} onChange={handleInputChange} style={{ padding: '10px', marginBottom: '20px', fontSize: '18px', width: '300px' }} />
         <button onClick={handleSendMessage} style={{ background: 'red', color: 'white', padding: '15px 30px', border: 'none', borderRadius: '5px', fontSize: '20px', cursor: 'pointer' }}>Pošalji</button>
@@ -63,7 +76,7 @@ const App = () => {
 
       {chatGptResponse && (
         <div style={{ marginTop: '20px', background: 'white', padding: '10px', borderRadius: '5px' }}>
-          <h3>Odgovor nasih pravnika:</h3>
+          <h3>Odgovor naših pravnika:</h3>
           <p>{chatGptResponse}</p>
         </div>
       )}
